01607bc1c917ecb7d2f8308f78d78a4f00d752a2
makeitgrowGIT <makeitgrow.in@gmail.com>
2023-06-01 20:57:28 +0530

    Renaming

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+//This is a rust learning project
+
 //This is a calculator app
 //It will list down all of the requried oprations with number of inputr required
 //Different module will host all of the functionality 
 //main.rs will act as UI
+
 
 fn main() {
     println!("Hello, world!");
