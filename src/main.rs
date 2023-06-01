ae848af5e921b45734a40469871eeb101c8e372e
makeitgrowGIT <makeitgrow.in@gmail.com>
2023-06-01 20:55:12 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+//This is a calculator app
+//It will list down all of the requried oprations with number of inputr required
+//Different module will host all of the functionality 
+//main.rs will act as UI
+
+fn main() {
+    println!("Hello, world!");
+}
